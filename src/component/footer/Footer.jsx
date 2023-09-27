77e29c986a3a77f005a68db442a5e6bc9df58314
kooroosh1363 <peymanraad1363@gmail.com>
2023-09-27 21:09:19 +0400

    add component code

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BsFacebook } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
 
 export default function Footer() {
   return (
-    <div>
-      Footer
+    <div className="footer">
+      <p>copy right @2023</p>
+      <div className="social">
+        <BsFacebook/>
+        <BsInstagram/>
+        <BsLinkedin/>
+      </div>
     </div>
   )
 }
--- src/component/mainContent/MainContent.jsx
+++ src/component/mainContent/MainContent.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import productCard from '../../data/pro_data';
 
-export default function MainContent() {
-  return (
-    <div>
-      MainContent
-    </div>
-  )
+const MainContent = () => {
+  console.log(productCard);
+  const ItemList = productCard.map((item)=>{})
 }
+
+export default MainContent;
